96490483fe49b6fd94fa7dd1e45722eb43e7545c
nicolas <nikolastesone@gmail.com>
2021-05-21 01:01:11 -0300

    classes created

---
+++ src/Classes/Passenger.java
@@ -0,0 +1,10 @@
+package Classes;
+
+import java.util.UUID;
+
+public class Passenger extends User {
+    private String Country, Street;
+    private boolean IsOldPassenger, IsActive;
+    private UUID Room;
+    //private Room ... revisar esta parte...
+}
